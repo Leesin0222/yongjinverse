d260062321e8e9d652e4e5ff21194d74c26bef94
lee <dragon02229@naver.com>
2024-09-21 22:46:50 +0900

    feat :: data, domain 모듈에 convention plugin 적용

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -1,43 +1,37 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.yongniverse.android.library)
+    alias(libs.plugins.yongniverse.hilt)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
     namespace = "com.yongjincompany.core.data"
-    compileSdk = 34
 
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+    flavorDimensions += "version"
+    productFlavors {
+        create("dev") {
+            dimension = "version"
+            buildConfigField("String", "BASE_URL", "\"https://c.com/\"")
         }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
+        create("stag") {
+            dimension = "version"
+            buildConfigField("String", "BASE_URL", "\"https://b.com/\"")
+        }
+        create("prod") {
+            dimension = "version"
+            buildConfigField("String", "BASE_URL", "\"https://a.com/\"")
+        }
     }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.kotlin.serialization)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging)
+
+    implementation(project(":core:domain"))
+    //implementation(project(":core:exception"))
 }
